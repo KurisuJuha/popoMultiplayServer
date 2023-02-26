381f3c2da9eaa66adf17db6fef08f84634fbda07
KYapp <kukiyomenaisapp@gmail.com>
2023-02-27 00:10:50 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace popoMultiplayServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
