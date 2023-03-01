6de38c7f32f047de7815b109525dd899849e62cb
KYapp <kukiyomenaisapp@gmail.com>
2023-03-01 22:15:41 +0900

    シングルスレッドで実行できるメインループクラスを作った

---
+++ MainLoop.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace JuhaKurisu.PopoTools.Multiplay.Server;
+
+public class MainLoop
+{
+    public readonly int fps;
+    private readonly Action action;
+    private readonly int sleepSpan;
+
+    public MainLoop(int fps, Action action)
+    {
+        this.fps = fps;
+        this.action = action;
+        sleepSpan = 1000 / fps;
+    }
+
+    public void Start()
+    {
+        while (true)
+        {
+            DateTime frameStartTime = DateTime.Now;
+            action.Invoke();
+            // フレーム終了時間まで待機
+            while (sleepSpan - (DateTime.Now.Microsecond - frameStartTime.Millisecond) > 0) { }
+        }
+    }
+}
+
