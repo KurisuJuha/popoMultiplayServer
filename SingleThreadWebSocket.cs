3144f5f7434e673f7de25ee2006cd196327c16fa
KYapp <kukiyomenaisapp@gmail.com>
2023-03-01 22:15:23 +0900

    SingleThreadでQueueにデータを保存しておくWebSocketを作った

---
+++ SingleThreadWebSocket.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Concurrent;
+using Fleck;
+
+namespace JuhaKurisu.PopoTools.Multiplay.Server;
+
+public class SingleThreadWebSocket : IDisposable
+{
+    public readonly ConcurrentQueue<IWebSocketConnection> onOpenQueue = new();
+    public readonly ConcurrentQueue<(byte[] bytes, IWebSocketConnection socket)> onBinaryQueue = new();
+    public readonly ConcurrentQueue<(string, IWebSocketConnection socket)> onMessageQueue = new();
+    public readonly ConcurrentQueue<IWebSocketConnection> onCloseQueue = new();
+    public readonly string location;
+    private readonly WebSocketServer server;
+
+    public SingleThreadWebSocket(string location)
+    {
+        this.location = location;
+        server = new(this.location);
+    }
+
+    public void Dispose()
+    {
+        server.Dispose();
+    }
+
+    public void Start()
+    {
+        server.Start(socket =>
+        {
+            socket.OnOpen += () => onOpenQueue.Enqueue(socket);
+            socket.OnBinary += bytes => onBinaryQueue.Enqueue((bytes, socket));
+            socket.OnMessage += message => onMessageQueue.Enqueue((message, socket));
+            socket.OnClose += () => onCloseQueue.Enqueue(socket);
+        });
+    }
+}
+
